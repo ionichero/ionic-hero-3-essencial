7683a77a00727e50a9c4a8d9061104f54b9b4775
Nanderson Castro <nandokstro@gmail.com>
2000-12-31 21:07:30 -0300

    Storage module

---
+++ ionic_storage_app/src/pages/edit/edit.ts
@@ -0,0 +1,35 @@
+import { Contact } from './../../model/contact';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import { StorageProvider } from './../../providers/storage/storage';
+
+/**
+ * Generated class for the EditPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-edit',
+  templateUrl: 'edit.html',
+})
+export class EditPage {
+
+  contact: Contact = {'email': '', 'name': '',  'phone': null};
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private storageProvider: StorageProvider) {
+      this.storageProvider.get(this.navParams.get('id'))
+                          .then((contact) => this.contact = contact);
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad EditPage');
+  }
+
+}
--- ionic_storage_app/src/pages/home/home.ts
+++ ionic_storage_app/src/pages/home/home.ts
@@ -1,6 +1,8 @@
+import { Contact } from './../../model/contact';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { StorageProvider } from '../../providers/storage/storage';
+import { EditPage } from '../edit/edit';
 
 @Component({
   selector: 'page-home',
@@ -8,11 +10,20 @@ import { StorageProvider } from '../../providers/storage/storage';
 })
 export class HomePage {
 
+  contacts: Contact[] = [];
+
   constructor(
     public navCtrl: NavController,
     private storageProvider: StorageProvider) {
+      this.storageProvider.getAll()
+                          .then((contacts) => this.contacts = contacts)
+                          .catch((err) => alert(err));
+  }
 
-      this.storageProvider.set('contact', {name: 'Nanderson', phone: 989889999, email: 'nandokstro@gmail.com'});
+  goToEditPage(id) {
+    this.navCtrl.push(EditPage, {
+      id: id
+    });
   }
 
 }
